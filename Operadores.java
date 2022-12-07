f167ea7585dd7858775713a7163b5d552d01547a
narahsouza <narahnsa@gmail.com>
2022-12-07 00:10:24 -0300

    Update

---
+++ Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+
+	// AQUI FICARÃO OS ATRIBUTOS DA CLASSE
+	int resultado;
+
+	// AQUI FICARÃO OS MÉTODOS DA CLASSE
+	public void somar(int valor1, int valor2) {
+		resultado = valor_1 + valor_2;
+	}
+
+	public void subtrair(int valor1, int valor2) {
+		resultado = valor_1 - valor_2;
+	}
+
+    public void multiplicar(int valor1, int valor2) {
+		resultado = valor_1 * valor_2;
+	}
+
+    public void dividir(int valor1, int valor2) {
+		resultado = valor_1 / valor_2;
+	}
+	
+}
