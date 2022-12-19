57ce8e7a783124d03888641222f9d5ad731e0d3b
narahsouza <narahnsa@gmail.com>
2022-12-19 19:54:36 -0300

    Update

---
+++ src/Apoio/Matematica.java
@@ -0,0 +1,24 @@
+package Apoio;
+
+public class Matematica {
+
+	// AQUI FICARÃO OS ATRIBUTOS DA CLASSE
+	public double resultado;
+
+	// AQUI FICARÃO OS MÉTODOS DA CLASSE
+	public void somar(double valor1, double valor2) {
+		resultado = valor1 + valor2;
+	}
+
+	public void subtrair(double valor1, double valor2) {
+		resultado = valor1 - valor2;
+	}
+
+    public void multiplicar(double valor1, double valor2) {
+		resultado = valor1 * valor2;
+	}
+
+    public void dividir(double valor1, double valor2) {
+		resultado = valor1 / valor2;
+	}
+}
